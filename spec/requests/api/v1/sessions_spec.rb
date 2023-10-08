840f48a63ed32a4c4d39c932f285486cf63a6ae3
DadaLV <vynohradova.l.a@gmail.com>
2023-10-08 19:28:45 +0200

    add user sign out

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'SessionsController', type: :request do
+  let(:user) { create :user }
   describe 'POST api/v1/auth/sign_in' do
-    let(:user) { create :user }
 
     it 'signs in a user successfully' do
       post '/api/v1/auth/sign_in', params: user.slice(:email, :password)
@@ -19,18 +19,8 @@ RSpec.describe 'SessionsController', type: :request do
     end
 
     it 'returns a valid authentication token when "Remember Me" option is checked' do
-      post '/api/v1/auth/sign_in', params: { email: user.email, password: user.password, remember_me: '1' }      
-      expect(response).to have_http_status(200)
-      expect(response.headers['access-token']).not_to be_nil
-
-      auth_tokens = {
-        'access-token': response.headers['access-token'],
-        'client': response.headers['client'],
-        'uid': response.headers['uid']
-      }
-
-      get '/api/v1/auth/validate_token', headers: auth_tokens  
-      expect(response).to have_http_status(200)  
+      post '/api/v1/auth/sign_in', params: { email: user.email, password: user.password, remember_me: '1' }
+      expect(user.reload.remember_expires_at > Time.zone.now).to be_truthy       
     end
 
     it 'returns a 401 unauthorized status when "Remember Me" option is not checked' do
@@ -41,6 +31,69 @@ RSpec.describe 'SessionsController', type: :request do
       get '/api/v1/auth/validate_token', params: { email: user.email, password: user.password }  
       expect(response).to have_http_status(401)  
     end
-    
   end
+
+  describe 'DELETE api/v1/auth/sign_out' do
+
+    it 'signs out a user successfully' do
+      post '/api/v1/auth/sign_in', params: user.slice(:email, :password)
+      expect(response).to have_http_status(200)
+
+      access_token = response.headers['access-token']
+      client = response.headers['client']
+      uid = response.headers['uid']
+
+      delete '/api/v1/auth/sign_out', headers: {
+        'access-token': access_token,
+        'client': client,
+        'uid': uid
+      }
+      expect(response).to have_http_status(200)
+
+      get '/api/v1/auth/validate_token', headers: {
+        'access-token': access_token,
+        'client': client,
+        'uid': uid
+      }
+
+      expect(response).to have_http_status(401)
+    end
+
+    it 'clears the user\'s remember token when signing out' do
+      
+      post '/api/v1/auth/sign_in', params: { email: user.email, password: user.password, remember_me: '1' }
+      expect(response).to have_http_status(200) 
+      expect(user.reload.remember_expires_at > Time.zone.now).to be_truthy
+      expect(user.reload.remember_expires_at).not_to be_nil
+      
+      
+      access_token = response.headers['access-token']
+      client = response.headers['client']
+      uid = response.headers['uid']
+      
+      get '/api/v1/auth/validate_token', headers: {
+        'access-token': access_token,
+        'client': client,
+        'uid': uid
+      }
+      expect(response).to have_http_status(200)
+
+      delete '/api/v1/auth/sign_out', headers: {
+        'access-token': access_token,
+        'client': client,
+        'uid': uid
+      }
+    
+      expect(response).to have_http_status(200)
+      
+      expect(user.reload.remember_created_at).to be_nil
+      get '/api/v1/auth/validate_token', headers: {
+        'access-token': access_token,
+        'client': client,
+        'uid': uid
+      }
+      expect(response).to have_http_status(401)
+    end
+  end
+
 end
